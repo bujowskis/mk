c8bf7ee5d657e94c3155a45871e11edcf019ecea
Szymon Bujowski <81238277+bujowskis@users.noreply.github.com>
2023-01-03 14:56:00 +0100

    add modular migration concept stuff

---
+++ framspy/evolalg/cs_base/migration/event_driven_migration.py
@@ -0,0 +1,6 @@
+"""
+Since event-drive migration brings new functionality (events), it should handle this itself.
+- access to previous population
+- access to new population
+- compare (check for events) -> act accordingly
+"""

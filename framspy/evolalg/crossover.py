d25eb28b31745823cb85dbf5d77852f134e148b7
Szymon Bujowski <81238277+bujowskis@users.noreply.github.com>
2023-02-06 19:49:36 +0100

    remove standard deviation from cec2017_numerical_crossover

--- framspy/evolalg/crossover.py
+++ framspy/evolalg/crossover.py
@@ -14,11 +14,10 @@ def simple_numerical_crossover(gen1, gen2):
     return output
 
 
-def cec2017_numerical_crossover(gen1, gen2, standard_deviation: float = 0.1):  # fixme - sd value
+def cec2017_numerical_crossover(gen1, gen2):  # NOTE - uniform -> no sd
     """
     Returns genotype that lies in a random place on a straight line drawn between parents
     """
-
     # https://stackoverflow.com/questions/55537838/given-two-points-of-4-or-more-dimensions-is-it-possible-to-find-a-line-equation
     straight_line_function = lambda g: np.array(gen1) + g * (np.array(gen2) - np.array(gen1))  # NOTE - g = 0.0 results in gen1, g = 1.0 results in gen2
     raw_individual = straight_line_function(random.uniform(0.0, 1.0))

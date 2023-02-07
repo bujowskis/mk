3ae53715f69e51ac00b1170ff025cfea7c736143
Sofya Aksenyuk <aksenyuk.sofya@gmail.com>
2023-02-07 16:00:27 +0100

    minor fix

--- framspy/evolalg/run_numerical_CSvsHFC.py
+++ framspy/evolalg/run_numerical_CSvsHFC.py
@@ -12,9 +12,9 @@ from cec2017.functions import f1, f3, f4, f5, f6, f7, f8, f9, f10, f21, f22, f23
 def main():
     NUMBER_OF_REPETITIONS = 30
     hof_size = 10
-    generations = 100_000
+    generations = 10
     functions = [f21, f22, f23, f24, f25, f26, f27, f28, f29, f30, f1, f3, f4, f5, f6, f7, f8, f9, f10]
-    dimensions = [2, 10, 20, 30, 50, 100]
+    dimension = 30
     parameters_default = {"migration_interval": 10, "populations": 25, "subpopsize": 50, "pmut": 0.8, "pxov": 0.2, "tournament_size": 5}
     parameters_optional = {"migration_interval": 2, "populations": 5, "subpopsize": 100, "pmut": 1.0, "pxov": 0.0, "tournament_size": 20}
     # [default, optional]
@@ -79,41 +79,39 @@ def main():
     for repetition in range(NUMBER_OF_REPETITIONS):
         for fraction in mutation_sd_fraction:
             for benchmark_function in functions:
-                for dimension in dimensions:
-                    migration_interval, number_of_populations, subpopsize, pmut, pxov, tournament_size = parameters_default.values()
+                migration_interval, number_of_populations, subpopsize, pmut, pxov, tournament_size = parameters_default.values()
 
-                    experiment = ExperimentNumericalCSEquiwidth(
-                        subpopsize, hof_size, number_of_populations, migration_interval,
-                        save_only_best=True, benchmark_function=benchmark_function, results_directory_path=results_directory_path,
-                        dimensions=dimension
-                    )
-                    hof, stats, df = experiment.evolve(
-                        hof_savefile=f'HoF/numerical_CSvsHFC/cs-ew/HoF-cs-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.gen',
-                        generations=generations,
-                        tournament_size=tournament_size,
-                        pmut=pmut,
-                        pxov=pxov,
-                        try_from_saved_file=False,
-                        initialgenotype=np.zeros(dimension)
-                    )
-                    df.to_csv(f'{results_directory_path}numerical_CSvsHFC-cs-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.csv')
+                experiment = ExperimentNumericalCSEquiwidth(
+                    subpopsize, hof_size, number_of_populations, migration_interval,
+                    save_only_best=True, benchmark_function=benchmark_function, results_directory_path=results_directory_path,
+                    dimensions=dimension
+                )
+                hof, stats, df = experiment.evolve(
+                    hof_savefile=f'HoF/numerical_CSvsHFC/cs-ew/HoF-cs-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.gen',
+                    generations=generations,
+                    tournament_size=tournament_size,
+                    pmut=pmut,
+                    pxov=pxov,
+                    try_from_saved_file=False,
+                    initialgenotype=np.zeros(dimension)
+                )
+                df.to_csv(f'{results_directory_path}numerical_CSvsHFC-cs-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.csv')
 
-                    experiment = ExperimentNumericalHFC(
-                        subpopsize, hof_size, number_of_populations, migration_interval,
-                        save_only_best=True, benchmark_function=benchmark_function, results_directory_path=results_directory_path,
-                        dimensions=dimension
-                    )
-                    hof, stats, df = experiment.evolve(
-                        hof_savefile=f'HoF/numerical_CSvsHFC/hfc/HoF-hfc-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.gen',
-                        generations=generations,
-                        tournament_size=tournament_size,
-                        pmut=pmut,
-                        pxov=pxov,
-                        try_from_saved_file=False,
-                        initialgenotype=np.zeros(dimension)
-                    )
-                    df.to_csv(f'{results_directory_path}numerical_CSvsHFC-hfc-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.csv')
-                    break
+                experiment = ExperimentNumericalHFC(
+                    subpopsize, hof_size, number_of_populations, migration_interval,
+                    save_only_best=True, benchmark_function=benchmark_function, results_directory_path=results_directory_path,
+                    dimensions=dimension
+                )
+                hof, stats, df = experiment.evolve(
+                    hof_savefile=f'HoF/numerical_CSvsHFC/hfc/HoF-hfc-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.gen',
+                    generations=generations,
+                    tournament_size=tournament_size,
+                    pmut=pmut,
+                    pxov=pxov,
+                    try_from_saved_file=False,
+                    initialgenotype=np.zeros(dimension)
+                )
+                df.to_csv(f'{results_directory_path}numerical_CSvsHFC-hfc-{benchmark_function.__name__}-{dimension}-{repetition}-{fraction}-{migration_interval}-{number_of_populations}-{subpopsize}-{pmut}-{pxov}-{tournament_size}.csv')
 
 
 if __name__ == "__main__":
